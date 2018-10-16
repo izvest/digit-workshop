437785ea738a58f81247a134b755bdd81dfc0762
Emil Holsti <eeehol@utu.fi>
2018-10-16 18:19:48 +0300

    tmp

--- digit_project/urls.py
+++ digit_project/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^da/', admin.site.urls),
+    path('da/', admin.site.urls),
 ]
 
 if settings.DEBUG:
