7f896daecb1d7d3e45104552e41e0fc99b353348
Emil Holsti <eeehol@utu.fi>
2018-10-24 17:33:05 +0300

    demottu versio

--- foli/views.py
+++ foli/views.py
@@ -25,10 +25,13 @@ def stop_detail(request, stop_id):
         response = requests.get(url).json()
         line1 = response["result"][0]["lineref"]
         time1 = round((response["result"][0]["expecteddeparturetime"]-response["result"][0]["recordedattime"])/60)
+        dest1 = response["result"][0]["destinationdisplay"]
         line2 = response["result"][1]["lineref"]
         time2 = round((response["result"][1]["expecteddeparturetime"]-response["result"][0]["recordedattime"])/60)
+        dest2 = response["result"][1]["destinationdisplay"]
         line3 = response["result"][2]["lineref"]
         time3 = round((response["result"][2]["expecteddeparturetime"]-response["result"][0]["recordedattime"])/60)
+        dest3 = response["result"][2]["destinationdisplay"]
     except:
         raise Http404('Stop does not exist')
     
@@ -36,8 +39,11 @@ def stop_detail(request, stop_id):
         'stop_id': stop_id,
         'line': line1,
         'departure': time1,
+        'dest1': dest1,
         'line2': line2,
         'departure2': time2,
+        'dest2': dest2,
         'line3': line3,
         'departure3': time3,
+        'dest3': dest3,
     })
