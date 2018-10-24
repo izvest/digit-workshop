652cd117cc0df61cd1283e77a8d9beb4a8a138c7
Emil Holsti <eeehol@utu.fi>
2018-10-24 12:39:03 +0300

    foli init

--- foli/views.py
+++ foli/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def home(request):
+    response = requests.get("http://data.foli.fi/siri/sm/T4/pretty").json()
+    line = response["result"][0]["lineref"]
+    time = round((response["result"][0]["expecteddeparturetime"]-response["result"][0]["recordedattime"])/60)
+    return render(request, 'foli/home.html', {
+        'line': line,
+        'departure': time
+    })
